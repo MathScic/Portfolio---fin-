07288d94aee821c76174b5e79224d413aadc4e74
Math Scicl <scicluna.mathieu@hotmail.fr>
2024-06-16 00:18:31 +0200

    modif 16/06

--- Competence/competence.js
+++ Competence/competence.js
@@ -1,33 +1,5 @@
 //Compétences
-// document.addEventListener("DOMContentLoaded", function () {
-//   const progressPercentages = {
-//     HTML: 90,
-//     CSS: 80,
-//     React: 60,
-//     JavaScript: 70,
-//     Node: 60,
-//   };
 
-//   const progressIDs = {
-//     HTML: "html-progress",
-//     CSS: "css-progress",
-//     React: "react-progress",
-//     JavaScript: "js-progress",
-//     Node: "node-progress",
-//   };
-
-//   for (let technologie in progressPercentages) {
-//     if (progressPercentages.hasOwnProperty(technologie)) {
-//       const progressBarID = progressIDs[technologie];
-//       const progressBar = document.getElementById(progressBarID);
-//       const percentage = progressPercentages[technologie];
-//       if (progressBar) {
-//         progressBar.style.width = `${percentage}%`;
-//         progressBar.textContent = `${percentage}%`;
-//       }
-//     }
-//   }
-// });
 let currentSlide = 0;
 let slideInterval;
 
